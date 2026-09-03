0ec43b9b13e067b3c9cb0afb8667fbec40b8e0de
christophe <studioideogramme@gmail.com>
2026-09-03 13:51:01 +0200

    Revert "Définir le rouge comme couleur par défaut"
    This reverts commit a2448d05d7582eb2e52ec474436278b56d415af0.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'couleurs-v3';
+const CACHE_NAME = 'couleurs-v2';
 const APP_FILES = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', (event) => {
